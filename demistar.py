8e42fbaf64270fc8c6aa58c4209337d84894778f
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-08-14 23:10:20 +0200

    app: extract code to a class

---
+++ demistar.py
@@ -0,0 +1,53 @@
+import network
+import time
+
+from neopixel import Neopixel
+
+WHEEL = [
+    (0xFF, 0x00, 0x00), (0xFF, 0x55, 0x00), (0xFF, 0xAA, 0x00),
+    (0xFF, 0xFF, 0x00), (0xAA, 0xFF, 0x00), (0x55, 0xFF, 0x00),
+    (0x00, 0xFF, 0x00), (0x00, 0xFF, 0x55), (0x00, 0xAA, 0xAA),
+    (0x00, 0x55, 0xFF), (0x00, 0x00, 0xFF), (0x00, 0x00, 0xFF),
+    (0x00, 0x00, 0xFF), (0x55, 0x00, 0xFF), (0xAA, 0x00, 0xFF),
+    (0xFF, 0x00, 0xFF)
+]
+
+class Demistar:
+    _net: network.WLAN
+    _rings: Neopixel
+
+    def init_network(self, ssid: str, psk: str, retries: int) -> bool:
+        self._net = network.WLAN(network.STA_IF)
+        self._net.active(True)
+        self._net.connect(ssid, psk)
+
+        for i in range(retries):
+            if self._net.status() < 0 or self._net.status() >= 3:
+                break
+
+            print("trying to connect ({i}/{max})".format(
+                i = i + 1,
+                max = retries))
+            time.sleep_ms(500)
+
+        return self._net.status() == 3
+
+    def init_rings(self, length: int, pin: int) -> None:
+        self._rings = Neopixel(length, 0, pin, "GRB")
+
+        index = 0
+        for pixel in WHEEL:
+            self._rings.set_pixel(index, (pixel[0] >> 1, pixel[1] >> 1, pixel[2] >> 1))
+            index = index + 1
+
+        for pixel in WHEEL:
+            self._rings.set_pixel(index, (pixel[0] >> 2, pixel[1] >> 2, pixel[2] >> 2))
+            index = index + 1
+
+    def run(self) -> None:
+        while True:
+            self._loop()
+
+    def _loop(self) -> None:
+        self._rings.show()
+        time.sleep(1)
--- main.py
+++ main.py
@@ -1,57 +1,16 @@
-import network
 import time
 
-from neopixel import Neopixel
-
 import config
+from demistar import Demistar
 
 
-# Static data
-WHEEL = [
-    (0xFF, 0x00, 0x00), (0xFF, 0x55, 0x00), (0xFF, 0xAA, 0x00),
-    (0xFF, 0xFF, 0x00), (0xAA, 0xFF, 0x00), (0x55, 0xFF, 0x00),
-    (0x00, 0xFF, 0x00), (0x00, 0xFF, 0x55), (0x00, 0xAA, 0xAA),
-    (0x00, 0x55, 0xFF), (0x00, 0x00, 0xFF), (0x00, 0x00, 0xFF),
-    (0x00, 0x00, 0xFF), (0x55, 0x00, 0xFF), (0xAA, 0x00, 0xFF),
-    (0xFF, 0x00, 0xFF)
-]
+app = Demistar()
 
-
-# Initialize the network interface
-net = network.WLAN(network.STA_IF)
-net.active(True)
-net.connect(config.IF_SSID, config.IF_PSK)
-
-for i in range(config.IF_TRY):
-    if net.status() < 0 or net.status() >= 3:
-        break
-
-    print("trying to connect...")
-    time.sleep_ms(500)
-
-if net.status() != 3:
-    raise RuntimeError('connection failed')
+if not app.init_network(config.IF_SSID, config.IF_PSK, config.IF_TRY):
+    print("connection failed")
 else:
-    print("connected to {ssid}, ipaddr = {addr}".format(
-        ssid = net.config('ssid'),
-        addr = net.ifconfig()[0]))
+    print("connected")
 
+app.init_rings(config.RING_PIXELS * config.RINGS, config.RINGS_PIN)
 
-# Initialize LED rings
-rings = Neopixel(config.RING_PIXELS * config.RINGS, 0, 11, "GRB")
-
-index = 0
-
-for pixel in WHEEL:
-    rings.set_pixel(index, (pixel[0] >> 1, pixel[1] >> 1, pixel[2] >> 1))
-    index = index + 1
-
-for pixel in WHEEL:
-    rings.set_pixel(index, (pixel[0] >> 2, pixel[1] >> 2, pixel[2] >> 2))
-    index = index + 1
-
-
-# Main program loop
-while True:
-    rings.show()
-    time.sleep(1)
+app.run()
