f0d5b1e489d563cca4c58f65c1ac5ea43c383b61
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-08-16 22:22:39 +0200

    app: initialize rings before the network

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from demistar import Demistar
 
 
 app = Demistar()
+app.init_rings(config.RING_PIXELS * config.RINGS, config.RINGS_PIN)
 
 if not app.init_network(config.IF_SSID, config.IF_PSK, config.IF_TRY):
     print("connection failed")
@@ -10,8 +11,6 @@ else:
     print("connected")
 
     addr = app.init_server(2137)
-    print("listening at {addr}".format(addr = addr))
-
-app.init_rings(config.RING_PIXELS * config.RINGS, config.RINGS_PIN)
+    print("listening at {}".format(addr))
 
 app.run()
