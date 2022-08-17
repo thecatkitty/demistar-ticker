8697a145ca3c8c58cb369573ee415901e3bb46c6
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-08-17 13:49:54 +0200

    api: extract ring controller

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
-import json
-import re
+import sys
+
+import controller
 
 from controller.ring import RingsProviderInterface
 from http import ContentProvider, HttpRequest, HttpResponse
-from view import ErrorView, JsonView
 
 
 class ApiProvider(ContentProvider):
@@ -14,48 +14,19 @@ class ApiProvider(ContentProvider):
         self._prings = rings_provider
 
     def handle_request(self, request: HttpRequest) -> HttpResponse:
-        if request.method not in ["GET", "POST"]:
+        parts = request.uri.split("/")
+        if parts[1] not in dir(controller):
+            return HttpResponse(404)
+
+        ctrl_name = parts[1][0].upper() + parts[1][1:] + "Controller"
+        try:
+            ctrl_class = getattr(sys.modules["controller.{}".format(parts[1])], ctrl_name)
+        except Exception as e:
+            print("api: ", str(e))
+            return HttpResponse(500)
+
+        if request.method.lower() not in dir(ctrl_class):
             return HttpResponse(405)
 
-        if request.uri.startswith("/ring"):
-            return self.handle_ring(request)
-
-        return HttpResponse(404)
- 
-    def handle_ring(self, request: HttpRequest) -> HttpResponse:
-        if request.method == "GET":
-            print("api: get ring pixel colors")
-            return JsonView({
-                "pixels": ["#{:06X}".format(rgb) for rgb in self.get_pixel_colors()]
-            }).render()
-
-        print("api: set ring pixel colors")
-        try:
-            data = json.loads(request.data.decode())
-        except ValueError as ve:
-            return ErrorView(422, str(ve)).render()
-
-        if type(data.get("pixels")) is not list:
-            return ErrorView(422, "'pixels' is expected to be a list").render()
-
-        pixels = data.get("pixels")
-        rings = self._prings.get_rings()
-        if len(pixels) > rings.num_leds:
-            return ErrorView(422, "'pixels' is too long").render()
-
-        for i, rgb in enumerate(pixels):
-            if type(rgb) is str and re.match("^#[0-9a-fA-F]+$", rgb):
-                print("api: set pixel {} color to {}".format(i, rgb))
-                rings.set_pixel(i,[int(rgb[i:(i + 2)], 16) for i in (1, 3, 5)])
-                self._prings.rings_changed()
-        return JsonView({}).render()
-
-    def get_pixel_colors(self):
-        rings = self._prings.get_rings()
-        rsh, gsh, bsh, _ = rings.shift
-        return [
-            (((pixel >> rsh) & 0xFF) << 16)
-                | (((pixel >> gsh) & 0xFF) << 8)
-                | ((pixel >> bsh) & 0xFF)
-            for pixel in rings.pixels
-        ]
+        ctrl = ctrl_class(self._prings)
+        return getattr(ctrl, request.method.lower())(request)
--- controller/ring.py
+++ controller/ring.py
@@ -1,4 +1,9 @@
+import json
+import re
+
+from http import HttpRequest, HttpResponse
 from neopixel import Neopixel
+from view import ErrorView, JsonView
 
 
 class RingsProviderInterface:
@@ -7,3 +12,48 @@ class RingsProviderInterface:
 
     def rings_changed(self) -> None:
         raise NotImplementedError()
+
+
+class RingController:
+    _prings: RingsProviderInterface
+
+    def __init__(self, rings_provider: RingsProviderInterface) -> None:
+        self._prings = rings_provider
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        print("api.ring: get")
+        return JsonView({
+            "pixels": ["#{:06X}".format(rgb) for rgb in self._get_pixel_colors()]
+        }).render()
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        print("api.ring: post")
+        try:
+            data = json.loads(request.data.decode())
+        except ValueError as ve:
+            return ErrorView(422, str(ve)).render()
+
+        if type(data.get("pixels")) is not list:
+            return ErrorView(422, "'pixels' is expected to be a list").render()
+
+        pixels = data.get("pixels")
+        rings = self._prings.get_rings()
+        if len(pixels) > rings.num_leds:
+            return ErrorView(422, "'pixels' is too long").render()
+
+        for i, rgb in enumerate(pixels):
+            if type(rgb) is str and re.match("^#[0-9a-fA-F]+$", rgb):
+                print("api: set pixel {} color to {}".format(i, rgb))
+                rings.set_pixel(i,[int(rgb[i:(i + 2)], 16) for i in (1, 3, 5)])
+                self._prings.rings_changed()
+        return JsonView({}).render()
+
+    def _get_pixel_colors(self):
+        rings = self._prings.get_rings()
+        rsh, gsh, bsh, _ = rings.shift
+        return [
+            (((pixel >> rsh) & 0xFF) << 16)
+                | (((pixel >> gsh) & 0xFF) << 8)
+                | ((pixel >> bsh) & 0xFF)
+            for pixel in rings.pixels
+        ]
