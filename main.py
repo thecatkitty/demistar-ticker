c6cba53a5748003c00dfc7ca48df4ea081cb2588
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-01-14 22:49:51 +0100

    main: display error message if connection fails

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ strip.show()
 
 top_display.draw_text("Demistar Ticker")
 top_display.update()
+bottom_display.clear()
+bottom_display.update()
 
 # Going online
 net = network.WLAN(network.STA_IF)
@@ -33,17 +35,26 @@ net.active(True)
 net.connect(IF_SSID, IF_PSK)
 
 for i in range(IF_TRY):
-    if net.status() < 0 or net.status() >= 3:
+    if net.status() >= network.STAT_GOT_IP:
         break
 
     print("net: connecting ({}/{})".format(i + 1, IF_TRY))
     inner_ring[i] = 0, 0, 64
     time.sleep(1)
 
-if net.status() != 3:
-    print("net: connection failed")
+status = net.status()
+if status != network.STAT_GOT_IP:
+    descriptions = {
+        network.STAT_IDLE: "network idle",
+        network.STAT_CONNECTING: "connecting",
+        network.STAT_CONNECT_FAIL: "connect fail",
+        network.STAT_NO_AP_FOUND: "no AP found",
+        network.STAT_WRONG_PASSWORD: "wrong password"}
+    description = descriptions[status] if status in descriptions.keys() else status
+
+    print("net: connection failed - {}".format(description))
     inner_ring.fill(64, 0, 0)
-    bottom_display.draw_text("no connection")
+    bottom_display.draw_text(description)
     bottom_display.update()
 
     time.sleep(15)
