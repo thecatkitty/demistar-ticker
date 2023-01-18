0905819acbbcf867b10f4f57af0b2f984d79f5d1
Mateusz Karcz <mateusz.karcz@interia.eu>
2023-01-18 22:14:39 +0100

    ticker: add the timeline

--- ticker/manager.py
+++ ticker/manager.py
@@ -14,14 +14,29 @@ class TimelineItem:
 
 
 class StageManager:
+    timeline: list[TimelineItem]
     cycle: list[TimelineItem]
     _index: int
     _cycle_start: int
 
     def __init__(self) -> None:
+        self.timeline = list()
         self.cycle = list()
         self._index = 0
         self._cycle_start = 0
+
+    def _get_cycle(self, now: int) -> None:
+        fresh = list(item for item in self.timeline if item.start < now)
+        for item in fresh:
+            print("manager: in - {}".format(item))
+            self.timeline.remove(item)
+            self.cycle.append(item)
+
+        stale = list(item for item in self.cycle
+                     if item.duration > 0 and item.start + item.duration < now)
+        for item in stale:
+            print("manager: out - {}".format(item))
+            self.cycle.remove(item)
 
     def _set_stage(self, index: int) -> None:
         self._index = index
@@ -41,10 +56,12 @@ class StageManager:
         self._set_stage(self._index)
 
     def handle(self) -> None:
+        now = utime.time()
+        self._get_cycle(now)
+
         if len(self.cycle) == 0:
             return
 
-        now = utime.time()
         if self._cycle_start == 0 or self._index >= len(self.cycle):
             return self._restart_cycle(now)
 
@@ -53,8 +70,10 @@ class StageManager:
 
         self.cycle[self._index].stage.update()
 
-    def add_stage(self, screentime: int, stage: Stage) -> None:
+    def add_stage(self, screentime: int, stage: Stage, start: int = 0, duration: int = 0) -> None:
         item = TimelineItem()
+        item.start = utime.time() if start == 0 else start
+        item.duration = duration
         item.screentime = screentime
         item.stage = stage
-        self.cycle.append(item)
+        self.timeline.append(item)
